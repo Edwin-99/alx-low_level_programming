e9b1bb18004091a3999f475c134775c8aa368853
Edwin <eaaboryo@st.ug.edu.gh>
2023-09-25 07:04:25 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,9 +10,9 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int x;
+	unsigned int x;
 
-  for (x = 0; x < n; ++x)
-    *(s + x) = b;
-  return (s);
+	for (x = 0; x < n; ++x)
+		s[x] = b;
+	return (s);
 }
